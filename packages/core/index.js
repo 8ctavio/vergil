e2c4742bec7f8b6365f025194101963d444ed0bf
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2023-04-07 21:13:52 +0200

    feat(styles): Add global-styles.css

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,7 +1,9 @@
+//---------- FUNCTIONS ----------
 export * from './components/alerts/alert'
 export * from './components/alerts/confirm'
 export * from './components/alerts/toast'
 
+//---------- COMPONENTS ----------
 export { default as AlertFeed } from './components/alerts/alert/AlertFeed.vue'
 export { default as Confirm } from './components/alerts/confirm/Confirm.vue'
 export { default as Toaster } from './components/alerts/toast/Toaster.vue'
