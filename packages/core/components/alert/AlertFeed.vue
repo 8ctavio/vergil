6dc470d2a6b26597d07eb0a7f7f3a82b4122f6eb
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2024-05-26 14:49:46 -0600

    feat(components): update alerts animations

--- packages/core/components/alert/AlertFeed.vue
+++ packages/core/components/alert/AlertFeed.vue
@@ -12,15 +12,16 @@ const feed = ref(null)
 onMounted(() => {
     feed.value = document.getElementById('alert-feed')
 })
-function onBeforeLeave(alert){
-    if(feed.value.children.length === 1){
-        alert.classList.add('lone')
-    }
-}
 </script>
 
 <template>
-    <TransitionGroup tag="div" id="alert-feed" name="alert" @before-leave="onBeforeLeave">
+    <TransitionGroup tag="div" id="alert-feed" name="alert"
+        @before-enter="() => feed.classList.add('enter')"
+        @after-enter="() => feed.classList.remove('enter')"
+        @before-leave="alert => {
+            alert.classList.add(feed.firstChild === alert ? 'first-leave' : 'not-first-leave')
+        }"
+        >
         <Alert v-for="alert in alertFeed" :key="alert.id"
             :message="alert.message"
             :details="alert.details"
@@ -34,6 +35,7 @@ function onBeforeLeave(alert){
 
 <style>
 #alert-feed{
+    --alert-gap: 15px;
     position: fixed;
     top: 0;
     left: 50%;
@@ -44,41 +46,49 @@ function onBeforeLeave(alert){
 
     & > .alert{
         margin: auto;
-        margin-top: 15px;
+        margin-top: var(--alert-gap);
 
+        /*-------- alert that is entering --------*/
         &:first-of-type{
             &.alert-enter-active{
                 transition: transform 500ms var(--bezier-bounce-out);
             }
             &.alert-enter-from{
-                /* translateY = -(alertsGap + height + alertShadow+1) */
-                transform: translateY(calc(-15px - 100% - 4px));
+                transform: translateY(calc((var(--alert-gap) + 100% + 4px) * -1));
             }
         }
-        &.lone{
+
+        /*-------- alert that is leaving --------*/
+        &.first-leave{
             &:is(.alert-move, .alert-leave-active){
                 transition: transform 500ms var(--bezier-bounce-in);
             }
             &.alert-leave-active{
                 position: absolute;
                 left: 50%;
-                transform: translateX(-50%) translateY(calc(-15px - 100% - 4px));
+                transform: translateX(-50%) translateY(calc((var(--alert-gap) + 100% + 4px) * -1));
             }
         }
-        &:not(:first-of-type, .lone){
-            &.alert-move{
-                transition: transform 400ms var(--bezier-sine-in-out), opacity 350ms;
-            }
-            &.alert-leave-active{
-                transition: transform 400ms var(--bezier-sine-out), opacity 350ms;
+        &.not-first-leave{
+            &:is(.alert-move, .alert-leave-active){
+                transition: transform 400ms var(--bezier-sine-in-out), opacity 350ms var(--bezier-sine-in-out);
             }
             &.alert-leave-active{
                 opacity: 0;
                 position: absolute;
                 left: 50%;
-                transform: translateX(-50%) translateY(-15px);
+                transform: translateX(-50%) translateY(calc(var(--alert-gap) * -0.7));
             }
         }
+
+        /*-------- alerts that move while an alert is leaving --------*/
+        &:not(.first-leave, .not-first-leave).alert-move{
+            transition: transform 500ms var(--bezier-bounce-in);
+        }
+    }
+    /*-------- alerts that move while an alert is entering --------*/
+    &.enter > .alert:not(:first-of-type).alert-move{
+        transition: transform 500ms var(--bezier-bounce-out);
     }
 }
 </style>
