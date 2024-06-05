76d4519ecef5207bdadcbcf14954385197d75d33
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2024-06-04 19:23:04 -0600

    feat(functions): add server specific utility functions

---
+++ packages/core/server/index.js
@@ -0,0 +1,47 @@
+import { getTimestamp } from '../functions'
+
+//----------------------------------------------
+//-------------------- DATE --------------------
+//----------------------------------------------
+/**
+ * Gets a start of day timestamp (00:00:00.000) of a timestamps' date in a given timezone.
+ * 
+ * @param { number } tzo - Timezone offset in minutes 
+ * @param { number } timestamp - Reference timestamp in milliseconds. Defaults to `Date.now()`
+ * @returns { number } Start-of-day timestamp of the target-timezone-date with the provided timestamp
+ */
+export function getDayStart(tzo, timestamp = Date.now()){
+    const dayUTC = new Date(getTimestamp({
+        from: timestamp,
+        offset: { d: Math.sign(-tzo), m: tzo } 
+    }))
+    dayUTC.setUTCHours(0,0,0,0)
+    return getTimestamp({
+        from: dayUTC.getTime(),
+        offset: { d: Math.sign(tzo), m: -tzo } 
+    })
+}
+
+//-----------------------------------------------
+//-------------------- ERROR --------------------
+//-----------------------------------------------
+/** Server specific (back-end) error */
+export class ServerError {
+    /**
+     * @param { string } code - [FunctionsErrorCode](https://firebase.google.com/docs/reference/node/firebase.functions#functionserrorcode)
+     * @param { string } message
+     * @param { {
+     *      operation: string
+     *      cause?: string
+     *      reason?: string
+     * } } details -
+     * - `operation`: Operation where error originated
+     * - `cause`: What caused the error
+     * - `reason`: Why was the error caused
+     */
+    constructor(code, message, details){
+        this.code = code
+        this.message = message
+        this.details = details
+    }
+}
