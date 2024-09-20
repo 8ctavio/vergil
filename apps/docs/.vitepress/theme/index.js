9a5062dedb20b487a7ac82eca8f90c8a93bcdf0f
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2024-09-19 21:08:08 -0600

    refactor(docs): update vitepress custom theme's layout with anonymous component

--- apps/docs/.vitepress/theme/index.js
+++ apps/docs/.vitepress/theme/index.js
@@ -1,17 +1,27 @@
 // https://vitepress.dev/guide/custom-theme
 import DefaultTheme from 'vitepress/theme'
+const { Layout } = DefaultTheme
+
 import './styles/custom.css'
 import './styles/overrides.css'
 
-import AppLayout from './components/AppLayout.vue'
 import Demo from './components/Demo.vue'
 import Anatomy from './components/Anatomy.vue'
 import ColorPicker from './components/ColorPicker.vue'
+import { PopupBackdrop, Confirm, Toasters } from '@8ctavio/vergil/components'
+import { defineComponent, h } from 'vue'
 import { vergil } from '@8ctavio/vergil/plugins'
 
 export default {
 	extends: DefaultTheme,
-	Layout: AppLayout,
+	Layout: defineComponent({
+		render: () => [
+			h(Layout),
+			h(PopupBackdrop),
+			h(Confirm),
+			h(Toasters)
+		]
+	}),
 	enhanceApp({ app, router, siteData }) {
 		app
 			.component('Demo', Demo)
