8cf2502d25301e8e9594f250a855c896f48a06df
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2024-11-12 11:28:29 -0600

    fix(components): remove popover jumpiness when scrolling

--- packages/core/components/PopoverPortal.vue
+++ packages/core/components/PopoverPortal.vue
@@ -4,15 +4,12 @@
 
 <style>
 #popover-portal {
-	position: fixed;
+	position: absolute;
 	top: 0;
 	left: 0;
 	z-index: var(--z-index-popover);
 
 	& > .popover-wrapper > .popover {
-		/* Fix for scale animation on Chrome */
-		transform-style: preserve-3d;
-        backface-visibility: hidden;
         &.popover-enter-active {
             transition: opacity 70ms var(--bezier-sine-out), transform 100ms var(--bezier-sine-out);
         }
@@ -21,7 +18,6 @@
         }
         &:is(.popover-enter-from, .popover-leave-to){
             opacity: 0;
-            /* transform: translateY(4px); */
             transform: scale(0.95);
         }
 	}
