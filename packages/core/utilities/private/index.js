a41b519238bd5ada9fc6ca2b4e9f87ba45577790
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2025-02-15 22:44:08 -0600

    refactor: move validators.js and inferTheme.js to theme.js

--- packages/core/utilities/private/index.js
+++ packages/core/utilities/private/index.js
@@ -3,9 +3,8 @@ export * from './directives'
 export * from './elements'
 export * from './events'
 export * from './FocusTrap'
-export * from './inferTheme'
 export * from './reactivity'
 export * from './tabbable'
-export * from './validators'
+export * from './theme'
 
 export const noop = () => {}
--- packages/core/utilities/private/theme.js
+++ packages/core/utilities/private/theme.js
@@ -1,3 +1,19 @@
+export function inferTheme(theme){
+    if(['brand'].includes(theme))
+        return 'brand'
+    if(['user'].includes(theme))
+        return 'user'
+    if(['ok', 'success', 'check'].includes(theme))
+        return 'ok'    
+    if(['info', 'help', 'tip'].includes(theme))
+        return 'info'
+    if(['warn', 'warning', 'caution'].includes(theme))
+        return 'warn'
+    if(['danger', 'error'].includes(theme))  
+        return 'danger'
+    return 'neutral'
+}
+
 export const isValidColor = v => ['cobalt', 'dartmouth', 'denim', 'emerald', 'grey', 'indigo', 'moss', 'red', 'sky', 'teal', 'wine', 'yellow'].includes(v)
 export const isValidRadius = v => ['none', 'sm', 'md', 'lg', 'full'].includes(v)
 export const isValidSize = v => ['xs', 'sm', 'md', 'lg', 'xl'].includes(v)
