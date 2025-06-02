48b5d09f6a31fc13b7ff694dbb488b5d9f5f1e25
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2025-06-01 20:15:23 -0600

    ci: configure vitest for SFC tests

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+	plugins: [vue()],
 	test: {
 		dir: 'tests',
 		environment: 'happy-dom',
 		typecheck: {
 			enabled: true,
+			checker: 'vue-tsc',
 			tsconfig: 'tsconfig.test.json'
 		}
 	}
