8f9fe1196b131b7be15172ab936e40bfc83e8935
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2025-07-27 19:30:50 -0600

    feat(utilities)!: remove getDayStart
    Converting dates between timezones through an static timezone offset is not reliable if the offset is associated to a geographical location's timezone (which is usually — if not always — the case). A proper implementation should use IANA timezone identifiers in order to consider DTS or other historical timezone changes for a given location.

--- apps/docs/src/functions/utilities.md
+++ apps/docs/src/functions/utilities.md
@@ -385,26 +385,6 @@ getTimestamp({
 })
 ```
 
-<!---------------------------------------------------
--------------------- getDayStart --------------------
----------------------------------------------------->
-### `getDayStart`
-
-> Gets a start of day timestamp (00:00:00.000) of another timestamps' date in a given timezone.
-
-```ts
-function getDayStart(tzo: number, timestamp?: number): number
-```
-
-#### Parameters
-
-- **`tzo`** — Timezone offset in minutes. Defaults to `Date.now()`.
-- **`timestamp`** — Reference timestamp in milliseconds.
-
-#### Return value
-
-Start-of-day timestamp of the target-timezone-date with the provided timestamp.
-
 ## Reactivity
 
 <!-----------------------------------------------------
--- packages/core/utilities/public.js
+++ packages/core/utilities/public.js
@@ -275,25 +275,6 @@ export function getTimestamp({ from = Date.now(), unit = 'ms', offset = {} } = {
 			: from + (delta * 1000)
 	)
 }
-
-/**
- * Gets a start of day timestamp (00:00:00.000) of another timestamps' date in a given timezone.
- * 
- * @param { number } tzo - Timezone offset in minutes 
- * @param { number } [timestamp = Date.now()] - Reference timestamp in milliseconds. Defaults to `Date.now()`
- * @returns { number } Start-of-day timestamp of the target-timezone-date with the provided timestamp
- */
-export function getDayStart(tzo, timestamp = Date.now()) {
-	const dayUTC = new Date(getTimestamp({
-		from: timestamp,
-		offset: { d: Math.sign(-tzo), m: tzo }
-	}))
-	dayUTC.setUTCHours(0, 0, 0, 0)
-	return getTimestamp({
-		from: dayUTC.getTime(),
-		offset: { d: Math.sign(tzo), m: -tzo }
-	})
-}
 // #endregion
 
 export { isWatchSource } from './reactivity'
