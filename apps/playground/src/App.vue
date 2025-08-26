f86096a1591c84eb05510a78d2772324b320f18f
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2025-08-26 17:08:33 -0600

    feat(playground): add dark-mode toggle and user-theme color picker

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import Header from '@/components/Header.vue'
 import { Vergil } from '@8ctavio/vergil/components'
 
 </script>
 
 <template>
-    <Vergil>
+    <Vergil confirm toaster popup>
+        <Header/>
         <main class="playground">
-
+            
         </main>
     </Vergil>
 </template>
---
+++ apps/playground/src/components/Header.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Switch } from '@8ctavio/vergil/components'
+import { ColorPicker } from '@8ctavio/vergil/utilities/userTheme'
+
+const checked = sessionStorage.getItem('playground-theme') === 'dark'
+if (checked) {
+	document.documentElement.classList.add('dark')
+} else {
+	document.documentElement.classList.remove('dark')
+}
+
+function toggleTheme() {
+	if (sessionStorage.getItem('playground-theme') === 'dark') {
+	    document.documentElement.classList.remove('dark')
+		sessionStorage.setItem('playground-theme', 'light')
+	} else {
+	    document.documentElement.classList.add('dark')
+		sessionStorage.setItem('playground-theme', 'dark')
+	}
+}
+</script>
+
+<template>
+	<header>
+		<ColorPicker/>
+		<Switch @change="toggleTheme" :checked icon-off="light_mode" icon-on="dark_mode" track="on"/>
+	</header>
+</template>
