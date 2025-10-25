4205a5b94ab06ba15366d5244d97f55cb2ed0e05
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2025-10-24 23:33:00 -0600

    refactor(playground): replace vite alias with package.json's import mapping

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Header from '@/components/Header.vue'
+import Header from '#src/components/Header.vue'
 import { Vergil } from '@8ctavio/vergil/components'
 
 </script>
