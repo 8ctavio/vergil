07e4afeb86a8c518340afa235ebca8cadaf12b5f
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2025-11-08 19:30:35 -0600

    fix(ModalBackdrop): always attach modal component event handlers

--- packages/core/src/components/modal/ModalBackdrop.js
+++ packages/core/src/components/modal/ModalBackdrop.js
@@ -3,13 +3,21 @@ import { ModalTransition } from "#components"
 import { modal, modalMeta, closeModal } from "./index.js"
 
 const handleClose = () => closeModal()
+const eventHandlers = {
+	onClose: handleClose,
+	onError: handleClose
+}
+
 export default function ModalBackdrop() {
 	return h(ModalTransition, {
 		id: 'modal-backdrop',
 		onAfterLeave: () => modalMeta.isLeaving = false,
 		show: modal.value.component !== null
-	}, () => modal.value.component && h(modal.value.component, modal.value.props && mergeProps(modal.value.props, {
-		onClose: handleClose,
-		onError: handleClose,
-	})))
+	}, () => modal.value.component && h(
+		modal.value.component,
+		modal.value.props
+			? mergeProps(modal.value.props, eventHandlers) 
+			: eventHandlers
+		)
+	)
 }
